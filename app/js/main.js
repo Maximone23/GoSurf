99c71a6267cfafee611dbf67816c52506a24ad57
Maxim Zalutskiy <maxim.one23@gmail.com>
2020-02-19 01:51:33 +0300

    Create header block

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+$('.header__slider').slick({
+  infinite: true,
+  fade: true,
+  prevArrow: '<img class="slider-arrows slider-arrows__left" src="./img/arrow-left.svg" alt="arrow">',
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="./img/arrow-right.svg" alt="arrow">',
+  asNavFor: '.slider-dots'
+});
+
+
+$('.slider-dots').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  asNavFor: '.header__slider'
+});
+
+  
+});
